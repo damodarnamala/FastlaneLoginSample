77c45dd76ed22fe014ec904edd17b5f51c6d7683
Damodar <damodar.namala@outlook.com>
2022-08-30 12:08:12 +0530

    Initial Commit

---
+++ StylesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StylesApp
+//
+//  Created by Damodar Namala on 30/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StylesApp/StylesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StylesAppApp.swift
+//  StylesApp
+//
+//  Created by Damodar Namala on 30/08/22.
+//
+
+import SwiftUI
+
+@main
+struct StylesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
